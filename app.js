d3351bd0149454df1b5804aee6d693150b6df336
Kamil Olszewski <kamil.web@gmail.com>
2019-12-26 13:52:15 +0100

    hangmanGame

--- app.js
+++ app.js
@@ -1,52 +1,43 @@
 const puzzleElement = document.querySelector("#puzzle");
 const guessesElement = document.querySelector("#guesses");
-const game1 = new Hangman("test", 3);
-
-puzzleElement.textContent = game1.puzzle;
-guessesElement.textContent = game1.statusMessage;
+let game;
 
 window.addEventListener("keypress", e => {
   const guess = String.fromCharCode(e.charCode);
-  game1.makeGuess(guess);
-  puzzleElement.textContent = game1.puzzle;
-  guessesElement.textContent = game1.statusMessage;
+  game.makeGuess(guess);
+  render();
 });
 
-getPuzzle("2", (error, puzzle) => {
-  if (error) {
-    console.log(`Error: ${error}`);
-  } else {
-    console.log(puzzle);
-  }
-});
+const render = () => {
+  puzzleElement.textContent = game.puzzle;
+  guessesElement.textContent = game.statusMessage;
+};
+
+const stratGame = async () => {
+  const puzzle = await getPuzzle("4");
+  game = new Hangman(puzzle, 4);
+  render();
+};
+
+document.querySelector("#reset").addEventListener("click", stratGame);
+
+stratGame();
 
 /*
-// Making an HTTP request
-const request = new XMLHttpRequest();
+getPuzzle("4")
+  .then(puzzle => {
+    console.log(puzzle);
+  })
+  .catch(err => {
+    console.log(`Error: ${err}`);
+  });
 
-request.addEventListener("readystatechange", e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    console.log(data);
-  }
-});
 
-request.open("GET", "http://puzzle.mead.io/puzzle");
-request.send();
-
-const countryCode = "US";
-const countryRequest = new XMLHttpRequest();
-
-countryRequest.addEventListener("readystatechange", e => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText);
-    const country = data.find(country => country.alpha2Code === countryCode);
+getCurrentCountry()
+  .then(country => {
     console.log(country.name);
-  } else if (e.target.readyState === 4) {
-    console.log("Unable to fetch data");
-  }
-});
-
-countryRequest.open("GET", "http://restcountries.eu/rest/v2/all");
-countryRequest.send();
+  })
+  .catch(error => {
+    console.log(error);
+  });
 */
--- requests.js
+++ requests.js
@@ -1,15 +1,38 @@
-const getPuzzle = (wordCount, callback) => {
-  const request = new XMLHttpRequest();
+const getPuzzle = async wordCount => {
+  const response = await fetch(
+    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+  );
 
-  request.addEventListener("readystatechange", e => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-      const data = JSON.parse(e.target.responseText);
-      callback(undefined, data.puzzle);
-    } else if (e.target.readyState === 4) {
-      callback("An error has taken place", undefined);
-    }
-  });
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error("Unable to fetch data");
+  }
+};
 
-  request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
-  request.send();
+const getCurrentCountry = async () => {
+  const location = await getLocation();
+  return getCountry(location.country);
+};
+
+const getCountry = async countryCode => {
+  const response = await fetch("http://restcountries.eu/rest/v2/all");
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.find(country => country.alpha2Code === countryCode);
+  } else {
+    throw new Error("Unable to fetch data");
+  }
+};
+
+const getLocation = async () => {
+  const response = await fetch("http://ipinfo.io/json?token=d4e8a4db4de6b7");
+
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error("Unable to fetch data");
+  }
 };
